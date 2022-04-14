b73532553aabf35e7e8749618e93314e15ac08e6
Abylay Aiyp <ablyaip@gmail.com>
2022-04-14 19:52:16 +0600

    init: with components

---
+++ src/components/Likes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import { incrementLikes, decrementLikes } from "../redux/actions";
+
+const Likes = (props) => {
+  return (
+    <div className="button-controls">
+      <button onClick={props.onIncrementLikes}>❤ {props.likes}</button>
+      <button onClick={props.onDecrementLikes}>Dislike</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const { likesReducer } = state;
+  return {
+    likes: likesReducer.likes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementLikes: () => dispatch(incrementLikes()),
+    onDecrementLikes: () => dispatch(decrementLikes()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Likes);
